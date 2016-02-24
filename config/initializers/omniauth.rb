8847df99f94bc6d04b046ded55ea0462eb8b17f1
lemonjai <leocheung_1986@hotmail.com>
2016-02-24 17:28:31 -0500

    add the yml for facebook, twitter & github

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter => Rails.root.join("config/twitter")
-  provider :facebook => Rails.root.join("config/facebook.yml")
-  provider :github  => Rails.root.join("config/github.yml")
+
+  FACEBOOK = YAML.load_file(Rails.root.join("config","facebook.yml"))
+  GITHUB = YAML.load_file(Rails.root.join("config","github.yml"))
+  TWITTER = YAML.load_file(Rails.root.join("config","twitter.yml"))
+
+  provider :facebook, FACEBOOK['app_id'], FACEBOOK['app_secret']
+  provider :github, FACEBOOK['app_id'], FACEBOOK['app_secret']
+  provider :twitter, FACEBOOK['app_key'], FACEBOOK['app_secret']
+
 end
