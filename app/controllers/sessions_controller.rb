814f62ce923934afdea0be6598dcb3a496d48475
lemonjai <leocheung_1986@hotmail.com>
2016-02-24 17:43:03 -0500

    add controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+	def create
+		auth = request.env["omniauth.auth"]
+		session[:omniauth] = auth.expect('extra')
+		user = User.sign_in_from_omniauth(auth)
+		session[:user_id] = user.user_id
+		redirect_to root_url, notice: "Sign In"
+	end
+
+	def destroy
+		session[:user_id] = nil
+		session[:omniauth] = nil
+		redirect_to root_url, notice: "Sign Out"
+	end
+
+end
