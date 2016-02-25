7f1bc92b2124e138da395d28735467abd1cbd289
lemonjai <leocheung_1986@hotmail.com>
2016-02-24 20:57:45 -0500

    testing

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,8 +4,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   GITHUB = YAML.load_file(Rails.root.join("config","github.yml"))
   TWITTER = YAML.load_file(Rails.root.join("config","twitter.yml"))
 
-  provider :facebook, FACEBOOK['app_id'], FACEBOOK['app_secret']
+  provider :facebook, '194350504263264', '57e795c09e6749b685f16efe882e0f27'
   provider :github, GITHUB['app_id'], GITHUB['app_secret']
-  provider :twitter, TWITTER['app_key'], TWITTER['app_secret']
+  provider :twitter, 'A9gGvEzdvHaE0PcZtYNNXavd8', 'RmGpdCKOmMUuaGLcUoL6NuzpYNrSCKRZ2n21U9dOipIuYo2tey'
 
 end
