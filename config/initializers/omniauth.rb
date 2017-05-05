59488dd41133e32dff6d86431c5c68c274b96234
Rodrigo <rodrigo19962010@live.com>
2017-05-05 17:53:06 -0300

    Autogit begin

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  FACEBOOK = YAML.load_file(Rails.root.join("config","facebook.yml"))
   GITHUB = YAML.load_file(Rails.root.join("config","github.yml"))
-  TWITTER = YAML.load_file(Rails.root.join("config","twitter.yml"))
 
-  provider :facebook, '194350504263264', '57e795c09e6749b685f16efe882e0f27'
-  provider :github, GITHUB['app_id'], GITHUB['app_secret']
-  provider :twitter, 'A9gGvEzdvHaE0PcZtYNNXavd8', 'RmGpdCKOmMUuaGLcUoL6NuzpYNrSCKRZ2n21U9dOipIuYo2tey'
+  provider :github, GITHUB['6ffde5aae55a3eb0ca4b'], GITHUB['db001b0445186e6a1d8ddb6e283848f44850da18']
 
 end
